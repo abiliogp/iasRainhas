68099ae08a4fef8d4f9f59105e62f07817fbe49a
elianes <eliane.sgt@gmail.com>
2012-05-04 22:07:42 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -6,7 +6,8 @@ import javax.swing.JFrame;
 /*
  * Trabalho para a disciplina de Inteligência Artificial - prof.: Anderson Ferrugem
  * 		Alunos: Abilio Gambim Parada
- * 				Vanderson Oliveira da Silva
+ * 			Vanderson Oliveira da Silva
+ *			Eliane Siegert
  */
 public class Main {
 	
@@ -21,19 +22,25 @@ public class Main {
 	
 	
 	//Precisa testar.......................
-	public int avalia_rainhas(int vet[]) {
+	static int avalia_rainhas(int vet[]) {
         int cont = 0; //contador de rainhas que se atacam
         for (int i = 0; i < 8; i++) {
             int x = 1;
             for (int j = i + 1; j < 8; j++) {  // avalia as diagonais à direita
-                if ((vet[j] == vet[i] + x) || (vet[j] == vet[i] - x)) {
+                  if(vet[i]==j){ //mesma coluna
+                    cont++;
+                }
+                if ((vet[j] == vet[i] + x) || (vet[j] == vet[i] - x) ||(vet[j]==vet[i])) {
                     cont++;
                 }
                 x++;
             }
             x = 1;
             for (int j = i - 1; j > 0; j--) {  //avalia as diagonais à esquerda
-                if ((vet[j] == vet[i] + x) || (vet[j] == vet[i] - x)) {
+               if(vet[i]==j){ //mesma coluna
+                    cont++;
+                }
+                if ((vet[j] == vet[i] + x) || (vet[j] == vet[i] - x) || (vet[j])==vet[i])) {
                     cont++;
                 }
                 x++;
