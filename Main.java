c3b68f7020be32458b66b778b31ad410bb14d429
Abilio Gambim Parada <abilio@positivo.(none)>
2012-05-15 21:16:20 -0300

    criado o método de geração de filhos incremental e testado

--- Main.java
+++ Main.java
@@ -12,12 +12,20 @@ import javax.swing.JFrame;
 public class Main {
 	
 	public static void main(String[] args) throws IOException {
+		int [] vet = {0,1,2,3,4,5,6,7};
+		Tabuleiro tab ,filho;
+		tab = new Tabuleiro(vet);
+		for(int i=0;i<65;i++){
+			filho = new Tabuleiro(tab);
+		}
+		/*
 		JFrame window = new JFrame("N-Rainhas IA");
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         window.setContentPane(new NrainhasUI());
         window.pack();  
         window.show();  
         window.setResizable(false);
+        */
 	}
 		
 }
--- Tabuleiro.java
+++ Tabuleiro.java
@@ -2,93 +2,101 @@ import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.TreeMap;
-    
-/* 
- * Esta classe possui todos os metodos necessários de um tabuleiro para resolução do problema das 8 rainhas
- * Autores: Vanderson Oliveira,Abilio Parada e Eliane.
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * 
+ * @author vanderson
  */
 public class Tabuleiro {
-    private int position[];
-    private int attack;
-    
-    public Tabuleiro(int entrada[]){
-       this.position=(entrada);//verificar se vai pegar certo
-       //metodo que verifica os ataques
-    }
+	private int[] board = new int[8];
+	private int attack = -1;
+	private Tabuleiro father;
+	private int count = 0;
 
-    Tabuleiro() {
-        throw new UnsupportedOperationException("Not yet implemented");
-    }
-    /* Autor: Vanderson Oliveira.
-     * Metodo get do numero de rainhas se atacando no tabuleiro
-     * P@r@metros: {void}
-     * Return:{int attack}
-     */
-    public int getAttack(){
-    	return this.attack;
-    }
-   
-    /*
-     * Função q de avaliação
-     */
-    public int avalia_rainhas() {
-        int []vet=this.position;
-        int cont = 0; //contador de rainhas que se atacam
-        for (int i = 0; i < 8; i++) {
-            int x = 1;
-            for (int j = i + 1; j < 8; j++) {  // avalia as diagonais à direita
-                 
-                if ((vet[j] == vet[i] + x) || (vet[j] == vet[i] - x) ||(vet[j]==vet[i])) {
-                    cont++;
-                }
-                x++;
-            }
-            x = 1;
-            for (int j = i - 1; j >= 0; j--) {  //avalia as diagonais à esquerda
-              
-                if ((vet[j] == vet[i] + x) || (vet[j] == vet[i] - x) || (vet[j] ==vet[i])) {
-                    cont++;
-                }
-                x++;
-            }
-        }
-        return cont;  //retorna quantas rainhas foram atacadas
-    }
-    /* Autor: Vanderson Oliveira.
-	 * Metodo que retorna o vetor com o tabuleiro. 
-	 * P@r@metros: {void}.
-	 * Return: {position}.
+	public Tabuleiro(int[] board) {
+		this.board = board;
+		this.father = father;
+	}
+
+	public Tabuleiro(Tabuleiro father) {
+		this.board = father.gerarFilho();
+		this.father = father;
+	}
+
+	/*
+	 * Metodo get do numero de rainhas se atacando no tabuleiro
 	 */
-    public int[] getPosition() {
-		// TODO Auto-generated method stub
-		return this.position;
+	public int getAttack() {
+		if (attack == -1) {
+			attack = avalia_rainhas();
+		}
+		return this.attack;
 	}
-    
-    public Tabuleiro expandeChildrens() {
-        Tabuleiro tabFilho = new Tabuleiro();
-         ArrayList<Object> arvore = null;
-         int [] vetFilho;
-         arvore.add(position);//aqui tem que ser o tabFilho pois precisamos de uma lista de objtos<Tabuleiro> (filhos)
-        
-        //gerar uma arvore com todos os filhos primeiro para depois cada algoritmo
-         //analise com cada nodo da arvore, mas qual será o tamanho dessa arvore e 
-         //que todos os nodos serão diferentes.
-         
-         
-        return tabFilho;
-    }
 
-    /*
-     * acho q temos q gerar um filho a cada vez a função é chamada
-     * para está função ficar uniforme para todos algoritmos
-     */
-	public void gerarFilhos() {
-		// TODO Auto-generated method stub
-		
+	/*
+	 * Função q de avaliação
+	 */
+	private int avalia_rainhas() {
+		int[] vet = this.board;
+		int cont = 0; // contador de rainhas que se atacam
+		for (int i = 0; i < 8; i++) {
+			int x = 1;
+			for (int j = i + 1; j < 8; j++) { // avalia as diagonais à direita
+
+				if ((vet[j] == vet[i] + x) || (vet[j] == vet[i] - x)
+						|| (vet[j] == vet[i])) {
+					cont++;
+				}
+				x++;
+			}
+			x = 1;
+			for (int j = i - 1; j >= 0; j--) { // avalia as diagonais à esquerda
+
+				if ((vet[j] == vet[i] + x) || (vet[j] == vet[i] - x)
+						|| (vet[j] == vet[i])) {
+					cont++;
+				}
+				x++;
+			}
+		}
+		return cont; // retorna quantas rainhas foram atacadas
 	}
-        
-        
-     
-    
-    
+
+	public Tabuleiro expandeChildrens() {
+		Tabuleiro tabFilho = new Tabuleiro();
+		ArrayList<Object> arvore = null;
+		int[] vetFilho;
+		arvore.add(board);
+
+		// gerar uma arvore com todos os filhos primeiro para depois cada
+		// algoritmo
+		// analise com cada nodo da arvore, mas qual será o tamanho dessa arvore
+		// e
+		// que todos os nodos serão diferentes.
+
+		return tabFilho;
+	}
+
+	/*
+	 * acho q temos q gerar um filho a cada vez a função é chamada para está
+	 * função ficar uniforme para todos algoritmos
+	 */
+	public int[] gerarFilho() {
+		int[] board = this.board.clone();
+		if (count >= 63) {
+			count = 0;
+		}
+		if (board[count / 8] == count % 8) {
+			count++;
+		}
+		board[count / 8] = count % 8;
+		count++;
+		return board;
+	}
+
 }
